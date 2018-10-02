d1b9dea8795f0352275982734b2590e406362377
G1edrelis <G1edrelis@users.noreply.github.com>
2018-10-02 10:51:28 +0300

    Gravity testas

---
+++ GravityTest/Assets/Scripts/PlayerComponent.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerComponent : MonoBehaviour {
+
+    public CharacterController2D controller;
+
+    Rigidbody2D rb;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+
+    bool jump = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        rb.gravityScale = -4.3f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            rb.gravityScale *= -1;
+        }
+
+	}
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        
+        jump = false;
+    }
+
+}
